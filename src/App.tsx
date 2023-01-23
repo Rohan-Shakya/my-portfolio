66fe06ccd12e88ad7a4b83703a8fd90ec4f85953
Rohan-Shakya <rohanshakya254@gmail.com>
2023-01-23 17:09:27 +0545

    added favicon and configure chakra ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,18 @@
-import * as React from "react"
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+import { Box, ChakraProvider, Flex, Text, theme } from "@chakra-ui/react";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
+    <Box background="black">
+      <Flex
+        as="main"
+        justifyContent="center"
+        flexDirection="column"
+        background="black"
+      >
+        <Text textAlign="center" fontSize="sm" color="white">
+          Rohan Shakya
+        </Text>
+      </Flex>
     </Box>
   </ChakraProvider>
-)
+);
